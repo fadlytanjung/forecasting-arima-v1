c7d82efd024b43afc40453ac8e7e8368fe5e5901
Fadly Tanjung <tanjungfadly96@gmail.com>
2019-12-11 22:12:27 +0700

    update heroku

--- app.py
+++ app.py
@@ -125,7 +125,7 @@ def predict_result():
     return render_template('predict_result.html',predict=dic_forecast)
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host="0.0.0.0", port=port)
+    # app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
